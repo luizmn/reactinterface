fd5a9e8b58a1570f420678bb831e9f691311c579
luizmn <luizmn@outlook.com>
2021-03-19 11:07:03 +0000

    Initial commit

---
+++ src/components/ListAppointments.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { MdPets } from 'react-icons/md';
+import Moment from 'react-moment';
+
+class ListAppointments extends Component {
+    render() {
+        return (
+            <div className="appointment-list item-list mb-3">
+                {this.props.appointments.map(item => (
+                    <div className="pet-item col media py-3" key={item.aptId}>
+                        <div className="mr-3">
+                            <button className="pet-delete btn btn-sm MdPets">
+                                <MdPets />
+                            </button>
+                        </div>
+
+                        <div className="pet-info media-body">
+                            <div className="pet-head d-flex">
+                                <span className="pet-name">{item.petName}</span>
+                                <span className="apt-date ml-auto">
+                                    <Moment
+                                        date={item.aptDate}
+                                        parse="YYYY-MM-dd hh:mm"
+                                        format="DD/MM/YYYY"
+                                    />
+                                </span>
+                            </div>
+
+                            <div className="owner-name">
+                                <span className="label-item">Owner: </span>
+                                <span>{item.ownerName}</span>
+                            </div>
+                            <div className="apt-notes">{item.aptNotes}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default ListAppointments;
